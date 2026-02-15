6051e27136851ffa5bca1e6dab29cb982c8501df
Alok Kurohane <alokshaniggc@gmail.com>
2026-02-15 12:52:10 +0530

    Update files.js

--- files.js
+++ files.js
@@ -1,7 +1,3 @@
 const jsonFiles = [
-    "alioth_hyperos_1.0.json",
-    "files.json",
-    "marble_hyperos_2.0.json",
-    "sweet_hyperos_china.json",
-    "wow.json"
+    "ginkgo_OS2_0_104_5.json"
 ];
